9bf245f823ba972aaf1a93ef6b3f5596d40b50f4
John R <24492257+pawn002@users.noreply.github.com>
2025-12-20 22:27:14 -0500

    feat: add Storybook story file for MapContainerComponent
    refs #10

---
+++ src/app/components/map-container/map-container.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { fn } from 'storybook/test';
+
+// import { MapContainerComponent } from './header.component';
+import { MapContainerComponent } from './map-container.component';
+import worldData from '../../data/world.json';
+import { GeoJsonObject, FeatureCollection } from 'geojson';
+
+const meta: Meta<MapContainerComponent> = {
+  title: 'Map/MapContainer',
+  component: MapContainerComponent,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  args: {},
+};
+
+export default meta;
+type Story = StoryObj<MapContainerComponent>;
+
+export const SVG: Story = {
+  args: {
+    width: 200,
+    height: 200,
+    geoData: worldData as FeatureCollection,
+    renderMode: 'svg',
+    // renderMode: 'svg',
+  },
+};
+
+export const Canvas: Story = {
+  args: {
+    width: 200,
+    height: 200,
+    geoData: worldData as FeatureCollection,
+    renderMode: 'canvas',
+    // renderMode: 'svg',
+  },
+};
